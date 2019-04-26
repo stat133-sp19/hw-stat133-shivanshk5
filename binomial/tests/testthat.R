a4db763ba2f44a25c4ae284cfb0e0d9d765dc990
Shivansh Kumar <Shivi@Ashoks-MBP.hsd1.ca.comcast.net>
2019-04-26 00:21:49 -0700

    Adding comments

--- binomial/tests/testthat.R
+++ binomial/tests/testthat.R
@@ -2,6 +2,7 @@
 # Author: Shivansh Kumar
 # Date: 1 May 2019
 # Description: Code loads required libraries for test functions on the loaded binomial package
+#              Also call devtools to run the tests in the testthat folder
 
 library(testthat)
 library(binomial)
